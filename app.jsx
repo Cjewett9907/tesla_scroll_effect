f56ab8ca0258535f3644b40be05bbd7933e1a268
Cjewett_work <Craig9907@gmail.com>
2019-08-05 07:06:00 -0700

    first commit functional code

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TeslaHero from './tesla_hero_object'
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(<TeslaHero />, document.getElementById('main'));
+});
---
+++ tesla_hero_object.jsx
@@ -0,0 +1,79 @@
+import React, {Component} from 'react'
+
+
+class TeslaHero extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            error: "RUT ROH",  
+            imageIdx: 0, 
+            message: "Nope", 
+            imageArray: ["div1","div2","div3","div4"], 
+            animating: false,
+        }
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+
+
+    componentDidMount() {
+        document.addEventListener('mousewheel', this.handleScroll ); 
+    }
+
+    componentWillUnmount(){
+        document.removeEventListener('mousewheel', this.handleScroll);
+    }
+
+    handleScroll(e) {
+       
+        // animating flag minimizes the actions of a scroll event via the setTimeout function
+        //  this is an attempt to counteract apple products 'kinetic mouse effect' where the mouse continues to scroll
+        //  long after the sweeping motion was initially made...
+            if(!this.state.animating){ 
+                this.setState({animating: true });
+
+                // simulated animation lag to slow the scrolling to 1 image at a time
+                setTimeout(() => {
+                    this.setState({animating: false});
+                }, 750)
+            
+            // if scrolling down
+                if(delta === -1){
+                    // and within bounds
+                    if(this.state.imageIdx < this.state.imageArray.length - 1){
+                        let imageIdx = this.state.imageIdx + 1
+                        this.setState({ imageIdx: imageIdx})
+                    }   
+            // if scrolling up 
+                } else if(delta === 1){
+                    // and within bounds
+                    if(this.state.imageIdx > 0){
+                        let imageIdx = this.state.imageIdx - 1
+                        this.setState({ imageIdx: imageIdx})
+                    }    
+                }
+                // determines if the mouse was scrolled up or down resulting in 1 or -1
+                var delta = Math.max(-1, Math.min(1, (e.wheelDelta)));
+
+                // scrollsBy height of window in the direction of the scroll
+                window.scrollBy({left: 0, top: (window.innerHeight * -delta), behavior: 'smooth'});
+            }
+    }
+
+    render(){
+        
+        return(
+            <div>
+               
+                <div className="div1"> </div>
+                <div className="div2"> </div>
+                <div className="div3"> </div>
+                <div className="div4"> </div>
+            </div>
+        );
+    }
+}
+
+export default TeslaHero
